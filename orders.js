a2331615cb7ddd4b09a779cd845f8091c58001ab
Shoaib <haq4402067@cloud.neduet.edu.pk>
2023-10-27 01:01:15 +0500

    Project completed

---
+++ orders.js
@@ -0,0 +1,7 @@
+const Orders = [
+  { productName: 'Ruko Fu Pro Drone', productNumber: '49347', paymentStatus: 'Due', shipping: 'Pending' },
+  { productName: 'Drone with Camera Drone', productNumber: '96996', paymentStatus: 'Paid', shipping: 'Declined' },
+  { productName: 'GPS 4k Drone', productNumber: '22821', paymentStatus: 'Paid', shipping: 'Delivered' },
+  { productName: 'DJI Air 2S', productNumber: '81475', paymentStatus: 'Due', shipping: 'Pending' },
+  { productName: 'Lozenge Drone', productNumber: '00482', paymentStatus: 'Paid', shipping: 'Delivered' },
+];
